5bc7f9d511585e7b1565d96ec020799b7a4d1c39
Benjamin DE GROOTE <benjamin.de-groote@niji.fr>
2018-09-12 11:14:34 +0200

    slow down again wheel

--- jquery.datetimepicker.js
+++ jquery.datetimepicker.js
@@ -895,7 +895,7 @@ var datetimepickerFactory = function ($) {
 				timeboxparent.on('mousewheel', function (event) {
 					var top = Math.abs(parseInt(timebox.css('marginTop'), 10));
 
-					top = top + (event.originalEvent.deltaY * 10);
+					top = top + (event.originalEvent.deltaY * 2);
 					if (top < 0) {
 						top = 0;
 					}
